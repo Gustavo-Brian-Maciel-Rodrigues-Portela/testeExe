e39d216c6ffd93bd281b1e92ca688a7602631f20
Gustavo-Brian-Maciel-Rodrigues-Portela <GustavoBrianGithub@gmail.com>
2023-06-19 10:53:58 -0300

    ok

---
+++ salvar.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <locale>
+#include <cstdlib>
+
+int main() {
+
+    
+    system("git add .");
+    system("git commit -m ok");
+    system("git push -u origin master");
+
+    return 0;
+}
